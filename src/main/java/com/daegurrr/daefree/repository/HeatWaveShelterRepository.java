ec76d0fc6dc30a1b749557c7b00bff9538887a6d
chaejm55 <chaejm55@gmail.com>
2024-09-22 04:49:40 +0900

    Feat: 쉼터 타입 조회 쿼리에 거리 조건 추가

--- src/main/java/com/daegurrr/daefree/repository/HeatWaveShelterRepository.java
+++ src/main/java/com/daegurrr/daefree/repository/HeatWaveShelterRepository.java
@@ -9,8 +9,20 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface HeatWaveShelterRepository extends JpaRepository<HeatWaveShelter, Long> {
-    @Query("SELECT new com.daegurrr.daefree.dto.shelter.ShelterResponse$Position(h.id, h.latitude, h.longitude) FROM HeatWaveShelter h WHERE h.restType = :restType")
-    List<ShelterResponse.Position> findByRestType(@Param("restType") String restType);
+    @Query("SELECT new com.daegurrr.daefree.dto.shelter.ShelterResponse$Position(h.id, h.latitude, h.longitude) " +
+            "FROM HeatWaveShelter h " +
+            "WHERE h.restType = :restType " +
+            "AND 6371000 * ACOS(COS(RADIANS(:latitude)) * COS(RADIANS(h.latitude)) "
+            + "* COS(RADIANS(h.longitude) - RADIANS(:longitude)) + "
+            + "SIN(RADIANS(:latitude)) * SIN(RADIANS(h.latitude))) <= :radius "
+            + "ORDER BY 6371000 * ACOS(COS(RADIANS(:latitude)) * COS(RADIANS(h.latitude)) "
+            + "* COS(RADIANS(h.longitude) - RADIANS(:longitude)) + "
+            + "SIN(RADIANS(:latitude)) * SIN(RADIANS(h.latitude))) ASC")
+    List<ShelterResponse.Position> findByRestTypeWithinRadius(
+            @Param("restType") String restType,
+            @Param("latitude") double latitude,
+            @Param("longitude") double longitude,
+            @Param("radius") double radius);
 
     @Query("SELECT new com.daegurrr.daefree.dto.shelter.ShelterResponse$Detail(h.id, h.restName, h.doroAddress, h.jibunAddress, h.deptContactNumber, " +
             "h.capacity, h.operationBeginDate, h.operationEndDate, h.openTime, h.closeTime, h.restType, h.latitude, h.longitude, h.isPublic) " +
