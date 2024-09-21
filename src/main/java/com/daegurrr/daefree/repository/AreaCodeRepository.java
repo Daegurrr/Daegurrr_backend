28ed3d048b0b48890833d7cce954f8b4cf0f7f88
chaejm55 <chaejm55@gmail.com>
2024-09-21 20:41:26 +0900

    Feat: 행정동 코드를 조회하기 위한 레포지토리 추가

---
+++ src/main/java/com/daegurrr/daefree/repository/AreaCodeRepository.java
@@ -0,0 +1,7 @@
+package com.daegurrr.daefree.repository;
+
+import com.daegurrr.daefree.entity.AreaCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AreaCodeRepository extends JpaRepository<AreaCode, String> {
+}
