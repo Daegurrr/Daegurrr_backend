af9f5220204f81ccf770609a5fa2fc409c90fdd7
chaejm55 <chaejm55@gmail.com>
2024-09-21 18:50:13 +0900

    Feat: 무더위 쉼터 검색 결과 반환을 위한 DTO 정의

---
+++ src/main/java/com/daegurrr/daefree/dto/SearchResponse.java
@@ -0,0 +1,39 @@
+package com.daegurrr.daefree.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+public class SearchResponse {
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Position {
+        private Double latitude;
+        private Double longitude;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Detail {
+        private Long id;
+        private String restName;
+        private String doroAddress;
+        private String jibunAddress;
+        private String deptContactNumber;
+        private int capacity;
+        private String operationBeginDate;
+        private String operationEndDate;
+        private String openTime;
+        private String closeTime;
+        private String restType;
+        private Double latitude;
+        private Double longitude;
+        private boolean isPublic;
+    }
+}
