b8a888d65906d2a867a667d387c83319fdcc494b
chaejm55 <chaejm55@gmail.com>
2024-09-21 18:12:56 +0900

    Feat: 무더위 쉼터 레포지토리 생성

---
+++ src/main/java/com/daegurrr/daefree/repository/HeatWaveShelterRepository.java
@@ -0,0 +1,7 @@
+package com.daegurrr.daefree.repository;
+
+import com.daegurrr.daefree.entity.HeatWaveShelter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HeatWaveShelterRepository extends JpaRepository<HeatWaveShelter, Long> {
+}
