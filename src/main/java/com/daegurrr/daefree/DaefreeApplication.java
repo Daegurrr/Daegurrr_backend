9c11de38ee550e5fa59f963116234b8b73763264
chaejm55 <chaejm55@gmail.com>
2024-09-21 10:56:30 +0900

    Initial commit

---
+++ src/main/java/com/daegurrr/daefree/DaefreeApplication.java
@@ -0,0 +1,13 @@
+package com.daegurrr.daefree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaefreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaefreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daegurrr/daefree/DaefreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daegurrr.daefree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaefreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
